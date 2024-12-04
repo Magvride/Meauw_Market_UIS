9189420c2f35c190cef3f99034061c47879e1857
Manuela Alejandra García Valbuena <97885177+Magvride@users.noreply.github.com>
2024-12-04 20:12:55 +0100

    Add files via upload

---
+++ proyecto-market/src/main/java/com/proyecto/proyecto_market/ProyectoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.proyecto_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//indica a spring que esta es la clase que tiene que ejecutar.
+@SpringBootApplication
+public class ProyectoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoMarketApplication.class, args);
+	}
+
+}

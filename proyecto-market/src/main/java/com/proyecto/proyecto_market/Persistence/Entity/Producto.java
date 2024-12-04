689b34e5f83422e83396b767b0fe837871609d66
Manuela Alejandra García Valbuena <manuela2211922@correo.uis.edu.co>
2024-12-04 20:53:02 +0100

    Conexion a la base de datos Creacion modelo productos

---
+++ proyecto-market/src/main/java/com/proyecto/proyecto_market/Persistence/Entity/Producto.java
@@ -0,0 +1,92 @@
+package com.proyecto.proyecto_market.Persistence.Entity;
+
+
+import jakarta.persistence.*;
+
+
+//La notación entity le dará a entender a java que se comporta como una
+//clase que mapea una tabla en nuestra base de datos
+@Entity
+@Table(name="productos")
+
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_product") //como cambiamos el nombre a como está en la tabla, toca especificar
+    private Integer idproducto;
+
+    private String nombre;
+
+    @Column(name="id_categoria")
+    private Integer idcategoria;
+
+    @Column(name="codigo_barras")
+    private String codigoBarras;
+
+    @Column(name="precio_venta")
+    private Double precioVenta;
+
+    @Column(name="cantidad_stock")
+    private Integer cantidadStock;
+
+    private Boolean estado;
+
+
+
+    private Integer getIdproducto() {
+        return idproducto;
+    }
+
+    private void setIdproducto(Integer idproducto) {
+        this.idproducto = idproducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getIdcategoria() {
+        return idcategoria;
+    }
+
+    public void setIdcategoria(Integer idcategoria) {
+        this.idcategoria = idcategoria;
+    }
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public Double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(Double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Integer getCantidadStock() {
+        return cantidadStock;
+    }
+
+    public void setCantidadStock(Integer cantidadStock) {
+        this.cantidadStock = cantidadStock;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}

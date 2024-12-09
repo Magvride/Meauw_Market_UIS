aefad97282a131c0d95b8d88e07ff0907b75d059
Manuela Alejandra García Valbuena <manuela2211922@correo.uis.edu.co>
2024-12-09 16:55:21 +0100

    creacion mapeo de datos

---
+++ proyecto-market/src/main/java/com/proyecto/proyecto_market/Domain/Repository/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.proyecto.proyecto_market.Domain.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import com.proyecto.proyecto_market.Domain.Product;
+
+public interface ProductRepository {
+
+    List<Product> getAll();
+    Optional<List<Product>> getByCategory();
+    Optional<List<Product>> getScaseProducts(int quantity);
+
+    //CRUD
+    Optional<Product> getProduct(int productId);
+    Product save(Product product);
+    void delete(int productId);
+}
---
+++ proyecto-market/src/main/java/com/proyecto/proyecto_market/Persistence/Mapper/CategoryMapper.java
@@ -0,0 +1,10 @@
+package com.proyecto.proyecto_market.Persistence.Mapper;
+import com.proyecto.proyecto_market.Domain.Category;
+import org.mapstruct.Mapper;
+
+
+
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+}

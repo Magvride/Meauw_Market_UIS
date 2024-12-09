fe3ed6f4fca5cfafc13e046cb09d0b19076c46af
Manuela Alejandra García Valbuena <manuela2211922@correo.uis.edu.co>
2024-12-09 16:25:27 +0100

    repository

--- proyecto-market/src/main/java/com/proyecto/proyecto_market/Persistence/ProductoRepository.java
+++ proyecto-market/src/main/java/com/proyecto/proyecto_market/Persistence/ProductoRepository.java
@@ -2,11 +2,15 @@ package com.proyecto.proyecto_market.Persistence;
 
 import com.proyecto.proyecto_market.Persistence.Crud.ProductoCrudRepository;
 import com.proyecto.proyecto_market.Persistence.Entity.Producto;
+import jakarta.persistence.Column;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+
+@Repository //indicando a spring que esta clase se encarga de interactuar con la BD
 public class ProductoRepository {
 
     private ProductoCrudRepository productoCrudRepository;
@@ -14,6 +18,7 @@ public class ProductoRepository {
     public Iterable<Producto> getAll(){
         return (ArrayList<Producto>) productoCrudRepository.findAll();
     }
+
 
     public List<Producto> getByCategoria(int idCategoria) {
         return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
@@ -24,5 +29,19 @@ public class ProductoRepository {
         return productoCrudRepository.findByIdCantidadStockLessThanAndEstado(cantidad,  true);
     }
 
+    //Obtener producto dado su ID
+    public Optional<Producto> getProducto(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
+    }
 
 }
